215ebe647a624edf20f7963028867f0120bfd699
SomiJo <jodeirsomaye@gmail.com>
2023-07-29 00:54:39 +0500

    Display the real forecast data

--- src/app.js
+++ src/app.js
@@ -20,24 +20,39 @@ function formqatDate(timestamp) {
   let day = days[date.getDay()];
   return `${day} ${hours}:${minutes}`;
 }
-
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+function displayForecast(responce) {
+  let forecast = responce.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
         <div class="weather-forecast-date">
-            ${day}
+            ${formatDay(forecastDay.time)}
                 </div>
-                     <img src="#" alt="Mostly-sunny" id="icon" width="40px" />
+                     <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                       forecastDay.condition.icon
+                     }" alt="${
+          forecastDay.condition.icon
+        }" id="icon" width="40px" />
                         <div class="weather-forecast-temperature">
-                            <span class="weather-forecast-temperature-max">18°</span>
-                             <span class="weather-forecast-temperature-min"> 12°</span>
+                            <span class="weather-forecast-temperature-max">${Math.round(
+                              forecastDay.temperature.maximum
+                            )}°</span>
+                             <span class="weather-forecast-temperature-min"> ${Math.round(
+                               forecastDay.temperature.minimum
+                             )}°</span>
                         </div>
          </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
 
